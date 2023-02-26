afa74d27fb5db20af83200460d68c8b9da5283ca
jeanetteftpurba <jftpurba@gmail.com>
2023-02-26 22:09:39 +0700

    update_login(2)

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -6,6 +6,8 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Auth;
+use App\Models\User;
+use App\Models\Postingan;
 
 
 class HomeController extends Controller
